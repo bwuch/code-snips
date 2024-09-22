694096612d6e9ac6e00394efaa21210329c87e30
Brian Wuchner <brian.wuchner@gmail.com>
2024-09-21 21:33:50 -0400

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -4,22 +4,22 @@ OUTFILE=/tmp/runme.sh
 echo '#!/bin/sh' > $OUTFILE
 
 echo -e '\n' >> $OUTFILE
-echo "echo This script is passing MAC address, hostname, and IP address to $SERVER for commands to execute on this specific client.  We will then sleep for 5 seconds and execute our combined script from $OUTFILE." >> $OUTFILE
+echo "echo This script is passing MAC address, hostname, and IP address to:\n   $SERVER \nfor commands to execute on this specific client.  \nWe will then sleep for 5 seconds and execute our combined script from \n$OUTFILE." >> $OUTFILE
 
 # Find MAC address
 read MAC </sys/class/net/eth0/address
 MAC=$(echo $MAC | sed 's/://g')
 echo -e "echo Commands specific to MAC address: $MAC \n" >> $OUTFILE
-curl $SERVER$MAC.txt -f >> $OUTFILE
+curl $SERVER$MAC.txt -fs >> $OUTFILE
 
 # IP address
 IPADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 echo -e "echo Commands specific to IP: $IPADDR \n" >> $OUTFILE
-curl $SERVER$IPADDR.txt -f >> $OUTFILE
+curl $SERVER$IPADDR.txt -fs >> $OUTFILE
 
 # Hostname
 echo -e "echo Commands specific to hostname: $(hostname) \n" >> $OUTFILE
-curl $SERVER$(hostname).txt -f >> $OUTFILE
+curl $SERVER$(hostname).txt -fs >> $OUTFILE
 
 # if we find any windows line breaks convert them to linux style
 sed -i.bak 's/\r$//' $OUTFILE
